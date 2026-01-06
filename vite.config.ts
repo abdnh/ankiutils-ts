0b8df4828696cb1e2c48c091a21282577359c333
Abdo <abdo@abdnh.net>
2026-01-06 07:12:02 +0300

    Remove lib/ subfolder

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,13 @@ import { dirname, resolve } from "node:path";
 import { fileURLToPath } from "node:url";
 import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
-import dts from 'vite-plugin-dts';
+import dts from "vite-plugin-dts";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [svelte(), dts({ tsconfigPath: './tsconfig.app.json' })],
+    plugins: [svelte(), dts({ tsconfigPath: "./tsconfig.app.json"})],
     build: {
         lib: {
             entry: resolve(__dirname, "src/index.js"),
