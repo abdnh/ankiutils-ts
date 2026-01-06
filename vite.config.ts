1d08f8936e008c681bb30635aed374377992eabc
Abdo <abdo@abdnh.net>
2026-01-06 23:58:17 +0300

    Convert to use official SvelteKit template
    Should have done that from the beginning and saved myself the headache...

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,6 @@
-import { dirname, resolve } from "node:path";
-import { fileURLToPath } from "node:url";
-import { defineConfig } from "vite";
-import { svelte } from "@sveltejs/vite-plugin-svelte";
-import dts from "vite-plugin-dts";
+import { sveltekit } from '@sveltejs/kit/vite';
+import { defineConfig } from 'vite';
 
-const __dirname = dirname(fileURLToPath(import.meta.url));
-
-// https://vite.dev/config/
 export default defineConfig({
-    plugins: [
-        svelte(),
-        dts({
-            tsconfigPath: "./tsconfig.app.json",
-            rollupTypes: true,
-        }),
-    ],
-    build: {
-        lib: {
-            entry: resolve(__dirname, "src/index.ts"),
-            name: "AnkiUtils",
-            fileName: "ankiutils",
-        },
-    },
+	plugins: [sveltekit()]
 });
