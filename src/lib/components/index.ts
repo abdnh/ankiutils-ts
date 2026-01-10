27b1ae48aaa225e165cdf76de72ecdede0c77ac3
Abdo <abdo@abdnh.net>
2026-01-10 03:02:49 +0300

    Export SelectOption

--- src/lib/components/index.ts
+++ src/lib/components/index.ts
@@ -1,7 +1,8 @@
 import Spinner from './Spinner.svelte';
+import type {SelectOption} from './SelectOptions.svelte';
 import Select from './Select.svelte';
 import MultiSelect from './MultiSelect.svelte';
 import Dropdown from './Dropdown.svelte';
 import ModalSelector from './ModalSelector.svelte';
 import KeyboardInput from './KeyboardInput.svelte';
-export { Spinner, Select, MultiSelect, Dropdown, ModalSelector, KeyboardInput };
+export { Spinner, type SelectOption, Select, MultiSelect, Dropdown, ModalSelector, KeyboardInput };
